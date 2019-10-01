da777646685dba490c27b171a3d43f6d11ad19a8
YujingGao <gao.yujing.csu@gmail.com>
2019-10-01 17:58:31 -0400

    better enhancement on about-us page

--- content/about-us.php
+++ content/about-us.php
@@ -4,10 +4,10 @@
             <img src="images/pic11.jpg" alt="" />
         </div>
         <header>
-            <h1>About Us - Team Gao</h1>
+            <h1>About Us</h1>
             <p class="info">Updated just now by <a href="#">Team Gao</a></p>
         </header>
-        <h2>We are Team Gao</h2>
+        <h2>We are <strong>Team Gao</strong></h2>
         <h4>Team members:</h4>
         <ul>
             <li>Yujing Gao</li>
