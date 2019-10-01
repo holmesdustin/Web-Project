d10a7b658a1b2a7e27954808cf32da49a3e435bc
YujingGao <gao.yujing.csu@gmail.com>
2019-10-01 17:31:43 -0400

    fix bugs that link wrong css

--- template/template.php
+++ template/template.php
@@ -1,34 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <title><?php page_title(); ?> | <?php site_name(); ?></title>
-    <link href="/template/style.css" rel="stylesheet" type="text/css" />
-</head>
-<body>
-<div class="wrap">
-
-    <header>
-        <h1><?php site_name(); ?></h1>
-        <nav class="menu">
-            <?php nav_menu(); ?>
-        </nav>
-    </header>
-
-    <article>
-        <h2><?php page_title(); ?></h2>
-        <?php page_content(); ?>
-    </article>
-
-    <footer>
-        <small>&copy;<?php echo date('Y'); ?> <?php site_name(); ?>.<br><?php site_version(); ?></small>
-    </footer>
-</div>
-</body>
-</html>
-
-
-
 <!DOCTYPE HTML>
 <!--
 	Binary by TEMPLATED
@@ -38,52 +7,49 @@
 <html>
 
 <head>
-	<title>Binary by TEMPLATED</title>
-	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<link rel="stylesheet" href="assets/css/main.css" />
+    <title><?php page_title(); ?> | <?php site_name(); ?></title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="template/assets/css/main.css" />
 </head>
 
 <body>
 
-	<!-- Header -->
-	<header id="header">
-		<a href="index.html" class="logo"><strong>Binary</strong> by TEMPLATED</a>
-		<nav>
-			<a href="#menu">Menu</a>
-		</nav>
-	</header>
+    <!-- Header -->
+    <header id="header">
+        <a href="index.html" class="logo"><strong>Binary</strong> by TEMPLATED</a>
+        <nav>
+            <a href="#menu">Menu</a>
+        </nav>
+    </header>
 
-	<!-- Nav -->
-	<nav id="menu">
-		<ul class="links">
-			<li><a href="index.html">Home</a></li>
-			<li><a href="generic.html">Generic</a></li>
-			<li><a href="elements.html">Elements</a></li>
-		</ul>
-	</nav>
+    <!-- Nav -->
+    <nav id="menu">
+        <ul class="links">
+        <?php nav_menu(); ?>
+        </ul>
+    </nav>
 
     <?php page_content(); ?>
 
-	<!-- Footer -->
-	<footer id="footer">
-		<ul class="icons">
-			<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-			<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-			<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-		</ul>
-		<div class="copyright">
-			&copy; Untitled. Design: <a href="https://templated.co">TEMPLATED</a>. Images: <a
-				href="https://unsplash.com">Unsplash</a>.
-		</div>
-	</footer>
+    <!-- Footer -->
+    <footer id="footer">
+        <ul class="icons">
+            <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+            <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+            <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+        </ul>
+        <div class="copyright">
+            &copy;<?php echo date('Y'); ?> <?php site_name(); ?>.<br><?php site_version(); ?>
+        </div>
+    </footer>
 
-	<!-- Scripts -->
-	<script src="assets/js/jquery.min.js"></script>
-	<script src="assets/js/jquery.scrolly.min.js"></script>
-	<script src="assets/js/skel.min.js"></script>
-	<script src="assets/js/util.js"></script>
-	<script src="assets/js/main.js"></script>
+    <!-- Scripts -->
+    <script src="template/assets/js/jquery.min.js"></script>
+    <script src="template/assets/js/jquery.scrolly.min.js"></script>
+    <script src="template/assets/js/skel.min.js"></script>
+    <script src="template/assets/js/util.js"></script>
+    <script src="template/assets/js/main.js"></script>
 
 </body>
 
