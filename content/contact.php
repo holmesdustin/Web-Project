2b13fab7088683b2a47c7900c546862109e9c62d
holmesdustin <holmes_dustin@columbusstate.edu>
2019-10-03 21:22:11 +0000

    contact

--- content/contact.php
+++ content/contact.php
@@ -10,11 +10,11 @@
         <h2>We are <strong>Team Gao</strong></h2>
         <h4>Team members:</h4>
         <ul>
-            <li>Yujing Gao <a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a> <a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+            <li>Yujing Gao &nbsp &nbsp &nbsp<a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a> <a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
 
-            <li>Caleb Corbin</li>
-            <li>Dustin Holmes</li>
-            <li>Johnathan Stadtmueller</li>
+            <li>Caleb Corbin  &nbsp &nbsp &nbsp<a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a> <a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+            <li>Dustin Holmes  &nbsp &nbsp &nbsp<a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a> <a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+            <li>Johnathan Stadtmueller &nbsp &nbsp &nbsp <a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a> <a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
         </ul>
     </div>
 </section>
