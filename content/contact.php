fb6f3774e864045bc8290ccf11e746e80484fa35
YujingGao <gao.yujing.csu@gmail.com>
2019-10-03 17:28:16 -0400

    adding github link to contacts

--- content/contact.php
+++ content/contact.php
@@ -10,11 +10,30 @@
         <h2>We are <strong>Team Gao</strong></h2>
         <h4>Team members:</h4>
         <ul>
-            <li>Yujing Gao <br><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a> <a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-
-            <li>Caleb Corbin  <br><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a> <a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-            <li>Dustin Holmes  <br><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a> <a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-            <li>Johnathan Stadtmueller <br><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a> <a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+            <li>Yujing Gao <br>
+                <a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a>
+                <a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a>
+                <a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a>
+                <a href="#" class="icon fa-github"><span class="label">Github</span></a>
+            </li>
+            <li>Caleb Corbin <br>
+                <a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a>
+                <a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a>
+                <a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a>
+                <a href="#" class="icon fa-github"><span class="label">Github</span></a>
+            </li>
+            <li>Dustin Holmes <br>
+                <a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a>
+                <a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a>
+                <a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a>
+                <a href="#" class="icon fa-github"><span class="label">Github</span></a>
+            </li>
+            <li>Johnathan Stadtmueller <br>
+                <a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a>
+                <a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a>
+                <a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a>
+                <a href="#" class="icon fa-github"><span class="label">Github</span></a>
+            </li>
         </ul>
     </div>
 </section>
