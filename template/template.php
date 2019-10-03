3a5cad033322572e8bb9eaaf95c496611773deea
pebblestad <stadtmueller.johnathan@yahoo.com>
2019-10-03 20:52:17 +0000

    reverted back'

--- template/template.php
+++ template/template.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="template/assets/css/main.css" />
-     <script src="https://kit.fontawesome.com/404832b999.js" crossorigin="anonymous"></script>
+    
 
 
 </head>
@@ -21,7 +21,7 @@
 
         <nav>
             <a href="#menu">Menu</a>
-            <i class="fas fa-film"></i>
+            
         </nav>
     </header>
 
