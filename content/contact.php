07af62ae4d55d1859f7eacf0639f8ac40f7599b0
YujingGao <gao.yujing.csu@gmail.com>
2019-10-04 02:07:53 -0400

    finished contact cards design

--- content/contact.php
+++ content/contact.php
@@ -21,14 +21,52 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xs-12 col-sm-6 col-lg-3">
 
+            <div class="col-xs-12 col-sm-6 col-lg-3">
+                <div class="card" style="100%;">
+                    <img class="card-img-top" src="https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616__340.jpg" alt="Caleb Corbin BG">
+                    <div class="card-body">
+                        <h5 class="card-title">Caleb Corbin</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a>
+                        <a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a>
+                        <a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a>
+                        <a href="#" class="icon fa-github"><span class="label">Github</span></a>
+                    </div>
+                </div>
             </div>
-            <div class="col-xs-12 col-sm-6 col-lg-3">
 
+            <div class="col-xs-12 col-sm-6 col-lg-3">
+                <div class="card" style="100%;">
+                    <img class="card-img-top" src="https://images.pexels.com/photos/949587/pexels-photo-949587.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Dustin Holmes BG">
+                    <div class="card-body">
+                        <h5 class="card-title">Dustin Holmes</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a>
+                        <a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a>
+                        <a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a>
+                        <a href="#" class="icon fa-github"><span class="label">Github</span></a>
+                    </div>
+                </div>
             </div>
-            <div class="col-xs-12 col-sm-6 col-lg-3">
 
+            <div class="col-xs-12 col-sm-6 col-lg-3">
+                <div class="card" style="100%;">
+                    <img class="card-img-top" src="https://media.istockphoto.com/photos/christmas-lights-defocused-background-bokeh-gold-blue-picture-id877684562?k=6&m=877684562&s=612x612&w=0&h=-WOtRrbuLnM_GlnDT7sw22w-8vZz8aPFasiVgxSG_eY=" alt="Johnathan Stadtmueller BG">
+                    <div class="card-body">
+                        <h5 class="card-title">Johnathan Stadtmueller</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a>
+                        <a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a>
+                        <a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a>
+                        <a href="#" class="icon fa-github"><span class="label">Github</span></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-lg-12">
+                <br>
+                <hr class="my-4"><br>
             </div>
 
             <div class="col-xs-12 col-sm-12 col-lg-12">
@@ -45,31 +83,5 @@
                 </div>
             </div>
         </div>
-        <ul>
-            <li>Yujing Gao <br>
-                <a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a>
-                <a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a>
-                <a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a>
-                <a href="#" class="icon fa-github"><span class="label">Github</span></a>
-            </li>
-            <li>Caleb Corbin <br>
-                <a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a>
-                <a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a>
-                <a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a>
-                <a href="#" class="icon fa-github"><span class="label">Github</span></a>
-            </li>
-            <li>Dustin Holmes <br>
-                <a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a>
-                <a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a>
-                <a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a>
-                <a href="#" class="icon fa-github"><span class="label">Github</span></a>
-            </li>
-            <li>Johnathan Stadtmueller <br>
-                <a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a>
-                <a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a>
-                <a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a>
-                <a href="#" class="icon fa-github"><span class="label">Github</span></a>
-            </li>
-        </ul>
     </div>
 </section>
