6469ee33cae0c3d187ba0848b38bd7d4af3178c4
YujingGao <gao.yujing.csu@gmail.com>
2019-10-04 02:33:46 -0400

    center form

--- content/contact.php
+++ content/contact.php
@@ -9,7 +9,7 @@
         <h3 class="junbotron" style="background-color: white">Contact us via Social Media:</h3>
         <div class="row container-fluid relative">
             <div class="col-xs-12 col-sm-6 col-lg-3">
-                <div class="card" style="100%; border-radius: 10px, 10px, 30px, 10px;">
+                <div class="card" style="100%; border-radius: 20px;">
                     <img class="card-img-top" src="https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616__340.jpg" alt="Yujing Gao BG">
                     <div class="card-body">
                         <h5 class="card-title">Yujing Gao</h5>
@@ -23,7 +23,7 @@
             </div>
 
             <div class="col-xs-12 col-sm-6 col-lg-3">
-                <div class="card" style="100%; border-radius: 10px, 10px, 30px, 10px;">
+                <div class="card" style="100%; border-radius: 20px;">
                     <img class="card-img-top" src="https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Caleb Corbin BG">
                     <div class="card-body">
                         <h5 class="card-title">Caleb Corbin</h5>
@@ -37,7 +37,7 @@
             </div>
 
             <div class="col-xs-12 col-sm-6 col-lg-3">
-                <div class="card" style="100%; border-radius: 10px, 10px, 30px, 10px;">
+                <div class="card" style="100%; border-radius: 20px;">
                     <img class="card-img-top" src="https://images.pexels.com/photos/949587/pexels-photo-949587.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Dustin Holmes BG">
                     <div class="card-body">
                         <h5 class="card-title">Dustin Holmes</h5>
@@ -51,7 +51,7 @@
             </div>
 
             <div class="col-xs-12 col-sm-6 col-lg-3">
-                <div class="card" style="100%; border-radius: 10px, 10px, 30px, 10px;">
+                <div class="card" style="100%; border-radius: 20px;">
                     <img class="card-img-top" src="https://media.istockphoto.com/photos/christmas-lights-defocused-background-bokeh-gold-blue-picture-id877684562?k=6&m=877684562&s=612x612&w=0&h=-WOtRrbuLnM_GlnDT7sw22w-8vZz8aPFasiVgxSG_eY=" alt="Johnathan Stadtmueller BG">
                     <div class="card-body">
                         <h5 class="card-title">Johnathan Stadtmueller</h5>
@@ -72,52 +72,52 @@
             <div class="col-xs-12 col-sm-12 col-lg-12">
                 <h3 class="junbotron" style="background-color: white">Contact us via Submitting Form :</h3>
             </div>
-
-            <form>
-                <div class="form-row">
-                    <div class="col-md-4 mb-3">
-                        <label for="validationDefault01">First name</label>
-                        <input type="text" class="form-control" id="validationDefault01" placeholder="First name" value="Mark" required>
-                    </div>
-                    <div class="col-md-4 mb-3">
-                        <label for="validationDefault02">Last name</label>
-                        <input type="text" class="form-control" id="validationDefault02" placeholder="Last name" value="Otto" required>
-                    </div>
-                    <div class="col-md-4 mb-3">
-                        <label for="validationDefaultUsername">Username</label>
-                        <div class="input-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text" id="inputGroupPrepend2">@</span>
+            <div class="col-xs-12 col-sm-12 col-lg-12">
+                <form style="width: 100%;">
+                    <div class="form-row">
+                        <div class="col-md-4 mb-3">
+                            <label for="validationDefault01">First name</label>
+                            <input type="text" class="form-control" id="validationDefault01" placeholder="First name" value="Mark" required>
+                        </div>
+                        <div class="col-md-4 mb-3">
+                            <label for="validationDefault02">Last name</label>
+                            <input type="text" class="form-control" id="validationDefault02" placeholder="Last name" value="Otto" required>
+                        </div>
+                        <div class="col-md-4 mb-3">
+                            <label for="validationDefaultUsername">Username</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroupPrepend2">@</span>
+                                </div>
+                                <input type="text" class="form-control" id="validationDefaultUsername" placeholder="Username" aria-describedby="inputGroupPrepend2" required>
                             </div>
-                            <input type="text" class="form-control" id="validationDefaultUsername" placeholder="Username" aria-describedby="inputGroupPrepend2" required>
                         </div>
                     </div>
-                </div>
-                <div class="form-row">
-                    <div class="col-md-6 mb-3">
-                        <label for="validationDefault03">City</label>
-                        <input type="text" class="form-control" id="validationDefault03" placeholder="City" required>
+                    <div class="form-row">
+                        <div class="col-md-6 mb-3">
+                            <label for="validationDefault03">City</label>
+                            <input type="text" class="form-control" id="validationDefault03" placeholder="City" required>
+                        </div>
+                        <div class="col-md-3 mb-3">
+                            <label for="validationDefault04">State</label>
+                            <input type="text" class="form-control" id="validationDefault04" placeholder="State" required>
+                        </div>
+                        <div class="col-md-3 mb-3">
+                            <label for="validationDefault05">Zip</label>
+                            <input type="text" class="form-control" id="validationDefault05" placeholder="Zip" required>
+                        </div>
                     </div>
-                    <div class="col-md-3 mb-3">
-                        <label for="validationDefault04">State</label>
-                        <input type="text" class="form-control" id="validationDefault04" placeholder="State" required>
+                    <div class="form-group">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
+                            <label class="form-check-label" for="invalidCheck2">
+                                Agree to terms and conditions
+                            </label>
+                        </div>
                     </div>
-                    <div class="col-md-3 mb-3">
-                        <label for="validationDefault05">Zip</label>
-                        <input type="text" class="form-control" id="validationDefault05" placeholder="Zip" required>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
-                        <label class="form-check-label" for="invalidCheck2">
-                            Agree to terms and conditions
-                        </label>
-                    </div>
-                </div>
-                <button class="btn btn-primary" type="submit">Submit form</button>
-            </form>
-
+                    <button class="btn btn-primary" type="submit">Submit form</button>
+                </form>
+            </div>
             <div class="col-xs-12 col-sm-12 col-lg-12">
                 <br>
                 <hr class="my-4"><br>
