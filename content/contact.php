780873261ae9c03b4fa7c9b285fc4ca6608a8d3f
YujingGao <gao.yujing.csu@gmail.com>
2019-10-04 02:13:33 -0400

    1. test for center icons 2. fix same picture error

--- content/contact.php
+++ content/contact.php
@@ -9,22 +9,24 @@
         <h4>Contact Us via Social Media:</h4>
         <div class="row container-fluid relative">
             <div class="col-xs-12 col-sm-6 col-lg-3">
-                <div class="card" style="100%;">
+                <div class="card" style="100%; border-radius: 20px">
                     <img class="card-img-top" src="https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616__340.jpg" alt="Yujing Gao BG">
                     <div class="card-body">
                         <h5 class="card-title">Yujing Gao</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a>
-                        <a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a>
-                        <a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a>
-                        <a href="#" class="icon fa-github"><span class="label">Github</span></a>
+                        <div class="container">
+                            <a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a>
+                            <a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a>
+                            <a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a>
+                            <a href="#" class="icon fa-github"><span class="label">Github</span></a>
+                        </div>
                     </div>
                 </div>
             </div>
 
             <div class="col-xs-12 col-sm-6 col-lg-3">
-                <div class="card" style="100%;">
-                    <img class="card-img-top" src="https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616__340.jpg" alt="Caleb Corbin BG">
+                <div class="card" style="100%; border-radius: 20px">
+                    <img class="card-img-top" src="https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Caleb Corbin BG">
                     <div class="card-body">
                         <h5 class="card-title">Caleb Corbin</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -37,7 +39,7 @@
             </div>
 
             <div class="col-xs-12 col-sm-6 col-lg-3">
-                <div class="card" style="100%;">
+                <div class="card" style="100%; border-radius: 20px">
                     <img class="card-img-top" src="https://images.pexels.com/photos/949587/pexels-photo-949587.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Dustin Holmes BG">
                     <div class="card-body">
                         <h5 class="card-title">Dustin Holmes</h5>
@@ -51,7 +53,7 @@
             </div>
 
             <div class="col-xs-12 col-sm-6 col-lg-3">
-                <div class="card" style="100%;">
+                <div class="card" style="100%; border-radius: 20px">
                     <img class="card-img-top" src="https://media.istockphoto.com/photos/christmas-lights-defocused-background-bokeh-gold-blue-picture-id877684562?k=6&m=877684562&s=612x612&w=0&h=-WOtRrbuLnM_GlnDT7sw22w-8vZz8aPFasiVgxSG_eY=" alt="Johnathan Stadtmueller BG">
                     <div class="card-body">
                         <h5 class="card-title">Johnathan Stadtmueller</h5>
