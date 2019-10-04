c2cb2750cbedc90f9395e4c7626430a08571665c
YujingGao <gao.yujing.csu@gmail.com>
2019-10-04 02:01:48 -0400

    testing for contact us card

--- content/contact.php
+++ content/contact.php
@@ -1,12 +1,50 @@
 <section id="banner">
-	<div class="inner">
-		<h1>Contact Us</h1>
-	</div>
+    <div class="inner">
+        <h1>Contact Us</h1>
+    </div>
 </section>
 
 <section id="main">
     <div class="inner">
         <h4>Contact Us via Social Media:</h4>
+        <div class="row container-fluid relative">
+            <div class="col-xs-12 col-sm-6 col-lg-3">
+                <div class="card" style="100%;">
+                    <img class="card-img-top" src="https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616__340.jpg" alt="Yujing Gao BG">
+                    <div class="card-body">
+                        <h5 class="card-title">Yujing Gao</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a>
+                        <a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a>
+                        <a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a>
+                        <a href="#" class="icon fa-github"><span class="label">Github</span></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-lg-3">
+
+            </div>
+            <div class="col-xs-12 col-sm-6 col-lg-3">
+
+            </div>
+            <div class="col-xs-12 col-sm-6 col-lg-3">
+
+            </div>
+
+            <div class="col-xs-12 col-sm-12 col-lg-12">
+                <div class="card">
+                    <div class="card-header">
+                        Quote
+                    </div>
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
+                            <p>Team Gao is the best team ever!</p>
+                            <footer class="blockquote-footer">Yujing Gao</footer>
+                        </blockquote>
+                    </div>
+                </div>
+            </div>
+        </div>
         <ul>
             <li>Yujing Gao <br>
                 <a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a>
