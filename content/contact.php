760fd0d5a2c42d5aa6ee733127e8b1835dc3a11f
YujingGao <gao.yujing.csu@gmail.com>
2019-10-04 01:43:58 -0400

    update contact page

--- content/contact.php
+++ content/contact.php
@@ -1,13 +1,14 @@
+<section id="banner">
+	<div class="inner">
+		<h1>Contact Us</h1>
+	</div>
+</section>
+
 <section id="main">
     <div class="inner">
-        <div class="image fit">
-            <img src="images/pic11.jpg" alt="" />
-        </div>
         <header>
-            <h1>Contact Us</h1>
             <p class="info">Updated just now by <a href="#">Team Gao</a></p>
         </header>
-        <h2>We are <strong>Team Gao</strong></h2>
         <h4>Team members:</h4>
         <ul>
             <li>Yujing Gao <br>
