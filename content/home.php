4eba1330196e297aa4423f238b95775b2af934b7
YujingGao <gao.yujing.csu@gmail.com>
2019-10-04 00:55:36 -0400

    delete the template for result showing

--- content/home.php
+++ content/home.php
@@ -12,47 +12,5 @@
 </section>
 
 <div id="result_showed" class="row container-fluid relative">
-	<div class="col-xs-12 col-sm-6 col-lg-3">
-		<div class="card" style="width: 100%;">
-			<img class="card-img-top img-fluid rounded" src="https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg" alt="Card image cap">
-			<div class="card-body">
-				<h5 class="card-title">Iron Man</h5>
-				<p class="card-text">After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.</p>
-				<a href="#" class="btn btn-primary">Read More</a>
-			</div>
-		</div>
-	</div>
-
-	<div class="col-xs-12 col-sm-6 col-lg-3">
-		<div class="card" style="width: 100%;">
-			<img class="card-img-top img-fluid rounded" src="https://m.media-amazon.com/images/M/MV5BMTM0MDgwNjMyMl5BMl5BanBnXkFtZTcwNTg3NzAzMw@@._V1_SX300.jpg" alt="Card image cap">
-			<div class="card-body">
-				<h5 class="card-title">Iron Man 2</h5>
-				<p class="card-text">With the world now aware of his identity as Iron Man, Tony Stark must contend with both his declining health and a vengeful mad man with ties to his father's legacy.</p>
-				<a href="#" class="btn btn-primary">Read More</a>
-			</div>
-		</div>
-	</div>
-
-	<div class="col-xs-12 col-sm-6 col-lg-3">
-		<div class="card" style="width: 100%;">
-			<img class="card-img-top img-fluid rounded" src="https://m.media-amazon.com/images/M/MV5BMjE5MzcyNjk1M15BMl5BanBnXkFtZTcwMjQ4MjcxOQ@@._V1_SX300.jpg" alt="Card image cap">
-			<div class="card-body">
-				<h5 class="card-title">Iron Man 3</h5>
-				<p class="card-text">When Tony Stark's world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution.</p>
-				<a href="#" class="btn btn-primary">Read More</a>
-			</div>
-		</div>
-	</div>
-
-	<div class="col-xs-12 col-sm-6 col-lg-3">
-		<div class="card" style="width: 100%;">
-			<img class="card-img-top img-fluid rounded" src="https://m.media-amazon.com/images/M/MV5BZDEyN2NhMjgtMjdhNi00MmNlLWE5YTgtZGE4MzNjMTRlMGEwXkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_SX300.jpg" alt="Card image cap">
-			<div class="card-body">
-				<h5 class="card-title">Spider-Man</h5>
-				<p class="card-text">When bitten by a genetically modified spider, a nerdy, shy, and awkward high school student gains spider-like abilities that he eventually must use to fight evil as a superhero after tragedy befalls his family.</p>
-				<a href="#" class="btn btn-primary">Read More</a>
-			</div>
-		</div>
-	</div>
+	
 </div>
