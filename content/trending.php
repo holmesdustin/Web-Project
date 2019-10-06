4a7f8467b6e640f2ef34bf851e8eef21dce24c67
YujingGao <gao.yujing.csu@gmail.com>
2019-10-05 22:00:33 -0400

    test

--- content/trending.php
+++ content/trending.php
@@ -6,11 +6,14 @@
 
 <div class="row container-fluid relative">
     <div class="col-xs-12 col-sm-12 col-lg-12">
+        <br>
+        <hr class="my-4">
+        <br>
         <h3 class="junbotron" style="background-color: white">Find out some Halloween specials:</h3>
     </div>
     <div class="col-xs-12 col-sm-6 col-lg-3">
-        <div class="card text-center" style="border-radius: 20px;">
-            <img class="card-img-top" style="border-radius: 20px 20px 0px 0px;" src="https://m.media-amazon.com/images/M/MV5BNzk1OGU2NmMtNTdhZC00NjdlLWE5YTMtZTQ0MGExZTQzOGQyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg" alt="BG">
+        <div class="card text-center" style="border-radius: 20px; height: 90%; width: 100%;">
+            <img class="card-img-top img-fluid" style="border-radius: 20px 20px 0px 0px;" src="https://m.media-amazon.com/images/M/MV5BNzk1OGU2NmMtNTdhZC00NjdlLWE5YTMtZTQ0MGExZTQzOGQyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg" alt="BG">
             <div class="card-body">
                 <h4 class="card-title">Halloween - 1978</h5>
                     <p class="card-text">Fifteen years after murdering his sister on Halloween night 1963, Michael Myers escapes from a mental hospital and returns to the small town of Haddonfield, Illinois to kill again.</p>
@@ -22,8 +25,8 @@
     </div>
 
     <div class="col-xs-12 col-sm-6 col-lg-3">
-        <div class="card text-center" style="border-radius: 20px;">
-            <img class="card-img-top" style="border-radius: 20px 20px 0px 0px;" src="https://m.media-amazon.com/images/M/MV5BMmQyYmY5ZTMtM2JkNi00NmM2LWE3ZmEtYWYzZmRkZDM0ZTdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg" alt="BG">
+        <div class="card text-center" style="border-radius: 20px; height: 90%; width: 100%;">
+            <img class="card-img-top img-fluid" style="border-radius: 20px 20px 0px 0px;" src="https://m.media-amazon.com/images/M/MV5BMmQyYmY5ZTMtM2JkNi00NmM2LWE3ZmEtYWYzZmRkZDM0ZTdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg" alt="BG">
             <div class="card-body">
                 <h4 class="card-title">Hocus Pocus - 1993</h5>
                     <p class="card-text">A curious youngster moves to Salem, where he struggles to fit in before awakening a trio of diabolical witches that were executed in the 17th century.</p>
@@ -35,8 +38,8 @@
     </div>
 
     <div class="col-xs-12 col-sm-6 col-lg-3">
-        <div class="card text-center" style="border-radius: 20px;">
-            <img class="card-img-top" style="border-radius: 20px 20px 0px 0px;" src="https://m.media-amazon.com/images/M/MV5BMTM3NjQyODI3M15BMl5BanBnXkFtZTcwMDM4NjM0OA@@._V1_SX300.jpg" alt="BG">
+        <div class="card text-center" style="border-radius: 20px; height: 90%; width: 100%;">
+            <img class="card-img-top img-fluid" style="border-radius: 20px 20px 0px 0px;" src="https://m.media-amazon.com/images/M/MV5BMTM3NjQyODI3M15BMl5BanBnXkFtZTcwMDM4NjM0OA@@._V1_SX300.jpg" alt="BG">
             <div class="card-body">
                 <h4 class="card-title">Hotel Transylvania - 2012</h5>
                     <p class="card-text">Dracula, who operates a high-end resort away from the human world, goes into overprotective mode when a boy discovers the resort and falls for the count's teenaged daughter.</p>
@@ -48,13 +51,19 @@
     </div>
 
     <div class="col-xs-12 col-sm-6 col-lg-3">
-        <div class="card text-center" style="border-radius: 20px;">
-            <img class="card-img-top" style="border-radius: 20px 20px 0px 0px;" src="https://m.media-amazon.com/images/M/MV5BOTAyYjQ3NjctNGMzNi00YThkLThmYzUtZDViYWZkMDA2YTMyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjAwMjc0NjQ@._V1_SX300.jpg" alt="BG">
+        <div class="card text-center" style="border-radius: 20px; height: 90%; width: 100%;">
+            <img class="card-img-top img-fluid" style="border-radius: 20px 20px 0px 0px;" src="https://m.media-amazon.com/images/M/MV5BOTAyYjQ3NjctNGMzNi00YThkLThmYzUtZDViYWZkMDA2YTMyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjAwMjc0NjQ@._V1_SX300.jpg" alt="BG">
             <div class="card-body">
                 <h4 class="card-title">Halloweentown - 1998</h5>
                     <p class="card-text">After learning she is a witch, a girl helps save a town full of other supernatural creatures.</p>
             </div>
         </div>
+        <br>
+        <hr class="my-4">
+        <br>
+    </div>
+
+    <div class="col-xs-12 col-sm-12 col-lg-12">
         <br>
         <hr class="my-4">
         <br>
