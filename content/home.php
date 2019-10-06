66bf34e1be37df6f6fa016a4c3f316ea65a0c48d
YujingGao <gao.yujing.csu@gmail.com>
2019-10-06 00:23:58 -0400

    make search bar looks better

--- content/home.php
+++ content/home.php
@@ -3,11 +3,12 @@
 	<div class="inner">
 		<h1>OMDb Movie Search Engine<br>
 			created by Team Gao</h1>
-		<input id="inputKey" type="text" placeholder="Title, Keyword..." aria-label="Search">
-		<br><br>
-		<ul class="actions">
-			<li><a id="button" href="#result_showed" class="button alt scrolly">Search</a></li>
-		</ul>
+
+		<div class="input-group md-form form-sm form-2 pl-0">
+			<input class="form-control" id="inputKey" type="text" placeholder="Search Movies by Title, Keyword..." aria-label="Search">
+			<button type="button" id="button" href="#result_showed"><i class="fas fa-search" aria-hidden="true"></i></button>
+		</div>
+		
 	</div>
 </section>
 <div id="result_showed" class="container-fluid relative row"></div>
