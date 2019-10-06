ba9666092cebc95ba3b2e0fdf1596ec611291801
YujingGao <gao.yujing.csu@gmail.com>
2019-10-06 00:59:42 -0400

    test

--- content/home.php
+++ content/home.php
@@ -5,7 +5,7 @@
 			created by Team Gao</h1>
 
 		<div class="input-group md-form form-sm form-2 pl-0">
-			<input class="form-control" id="inputKey" type="text" placeholder="Search Movies by Title, Keyword..." aria-label="Search">
+			<input class="form-control" style="font-size:unset;" id="inputKey" type="text" placeholder="Search Movies by Title, Keyword..." aria-label="Search">
 			<button type="button" id="button" href="#result_showed"><i class="fas fa-search" aria-hidden="true"></i></button>
 		</div>
 		
