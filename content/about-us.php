1dd1097e3b63e4dd6c927c465d4e59ac94998761
YujingGao <gao.yujing.csu@gmail.com>
2019-10-06 18:36:36 -0400

    test

--- content/about-us.php
+++ content/about-us.php
@@ -26,7 +26,7 @@
 
             Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
         </h5>
-        <br><h class="my-4"><br>
+        <br><hr class="my-4"><br>
         <h2 class="text-center">Team members</h2>
         <div class="row container-fluid relative">
             <div class="col-xs-12 col-sm-6 col-lg-3">
