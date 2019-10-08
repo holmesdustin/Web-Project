73e27ffc8123d8244f06995241dc29f430054486
YujingGao <gao.yujing.csu@gmail.com>
2019-10-07 22:20:30 -0400

    test

--- includes/functions.php
+++ includes/functions.php
@@ -1,10 +1,8 @@
 <?php
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
-
-// Load Composer's autoloader
-require 'vendor/autoload.php';
 
 if (isset($_POST["search"])) {
     searchByKeyword($_POST["search"]);
@@ -27,39 +25,31 @@ function emailTeamMessage($firstName, $lastName, $email, $message)
 {
     $mail = new PHPMailer(true);
 
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'noreply.teamgao@gmail.com';            // SMTP username
-    $mail->Password   = 'P@ssw0rdtoor';                               // SMTP password
-    $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-    $mail->Port       = 587;                                    // TCP port to connect to
+    try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+        $mail->isSMTP();                                            // Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                       // Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+        $mail->Username   = 'noreply.teamgao@gmail.com';            // SMTP username
+        $mail->Password   = 'P@ssw0rdtoor';                               // SMTP password
+        $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+        $mail->Port       = 587;                                    // TCP port to connect to
 
-    //Recipients
-    $mail->setFrom('noreply.teamgao@gmail.com', 'Team Gao');
-    $mail->addAddress('gao_yujing@columbusstate.edu', 'Yujing Gao');     // Add a recipient
-    //$mail->addReplyTo('info@example.com', 'Information');
-    //$mail->addCC('cc@example.com');
-    //$mail->addBCC('bcc@example.com');
+        //Recipients
+        $mail->setFrom('noreply.teamgao@gmail.com', 'Team Gao');
+        $mail->addAddress('gao_yujing@columbusstate.edu', 'Yujing Gao');     // Add a recipient
 
-    // Attachments
-    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'Here is the subject';
+        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    echo 'Thank you! Your message has been sent to our team.';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
+        $mail->send();
+        echo 'Thank you! Your message has been sent to our team.';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
 }
 
 /**
@@ -116,14 +106,12 @@ function parseKeyword($keyword)
     $keyword = trim($keyword);
     $keywordPieces = explode(" ", $keyword);
     $keyword = "";
-    for ($x = 0; $x < sizeof($keywordPieces)-1; $x++) {
-        if ($keywordPieces[$x] != "")
-        {
+    for ($x = 0; $x < sizeof($keywordPieces) - 1; $x++) {
+        if ($keywordPieces[$x] != "") {
             $keyword = $keyword . $keywordPieces[$x] . "+";
         }
-        
     }
-    $keyword = $keyword . $keywordPieces[sizeof($keywordPieces)-1];
+    $keyword = $keyword . $keywordPieces[sizeof($keywordPieces) - 1];
     return $keyword;
 }
 
