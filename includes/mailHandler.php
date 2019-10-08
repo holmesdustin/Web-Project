2f32eb8de8571ced4552cee589a1a2abf3727a23
YujingGao <gao.yujing.csu@gmail.com>
2019-10-07 23:59:32 -0400

    ready for testing

--- includes/mailHandler.php
+++ includes/mailHandler.php
@@ -1,48 +1,47 @@
 <?php
 
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 require 'includes/config.php';
-require 'includes/PHPMailer/src/Exception.php';
-require 'includes/PHPMailer/src/PHPMailer.php';
-require 'includes/PHPMailer/src/SMTP.php';
+// Load Composer's autoloader
+require 'vendor/autoload.php';
 
 $emailFrom = "";
 $firstName = "";
 $LastName = "";
 $message = "";
 
-if (isset($_POST["mailInfoSet"])) {
-    $emailFrom = $_POST["email"];
-    $firstName = $_POST["firstName"];
-    $LastName = $_POST["lastName"];
-    $message = $_POST["message"];
 
-    $mail = new PHPMailer(true);
+$emailFrom = $_POST["email"];
+$firstName = $_POST["firstName"];
+$LastName = $_POST["lastName"];
+$message = $_POST["message"];
 
-    try {
-        //Server settings
-        $mail->isSMTP();                                            // Send using SMTP
-        $mail->Host       = config('email')['host'];                    // Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = config('email')['username'];                     // SMTP username
-        $mail->Password   = config('email')['password'];                               // SMTP password
-        $mail->SMTPSecure = config('email')['SMTPSecure'];  'ssl';       // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-        $mail->Port       = config('email')['port'];                                    // TCP port to connect to
-        
-        //Recipients
-        $mail->setFrom($emailFrom, $firstName . ' ' . $LastName);
-        $mail->addAddress('gao_yujing@columbusstate.edu', 'Yujing Gao');     // Add a recipient
+$mail = new PHPMailer(true);
 
-        $mail->isHTML(true);                                  // Set email format to HTML
-        $mail->Subject = 'You got a message';
-        $mail->Body    = '<p>' . $message . '</p>';
-        $mail->AltBody = $message;
+try {
+    //Server settings
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = config['email']['host'];                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = config['email']['username'];                     // SMTP username
+    $mail->Password   = config['email']['password'];                               // SMTP password
+    $mail->SMTPSecure = config['email']['SMTPSecure'];      // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = config['email']['port'];                                    // TCP port to connect to
 
-        $mail->send();
-        echo 'Thank you! Your message has been sent to our team.';
-    } catch (Exception $e) {
-        echo "Message could not be sent.";
-    }
-};
+    //Recipients
+    $mail->setFrom($emailFrom, $firstName . ' ' . $LastName);
+    $mail->addAddress('gao_yujing@columbusstate.edu', 'Yujing Gao');     // Add a recipient
+
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'You got a message';
+    $mail->Body    = '<p>' . $message . '</p>';
+    $mail->AltBody = $message;
+
+    $mail->send();
+    echo 'Thank you! Your message has been sent to our team.';
+} catch (Exception $e) {
+    echo "Message could not be sent.";
+}
