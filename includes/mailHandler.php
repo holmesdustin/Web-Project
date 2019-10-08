39624fac110b01256b8aa28d06f7ccdd1cd2dc3c
YujingGao <gao.yujing.csu@gmail.com>
2019-10-08 01:24:21 -0400

    /

--- includes/mailHandler.php
+++ includes/mailHandler.php
@@ -22,6 +22,7 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;   
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = config['email']['host'];                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
