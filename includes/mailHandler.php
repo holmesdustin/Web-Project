9b44d076fa9bbe152e677c2892600f0f6f9ab5ad
YujingGao <gao.yujing.csu@gmail.com>
2019-10-08 02:28:46 -0400

    fail

--- includes/mailHandler.php
+++ includes/mailHandler.php
@@ -13,26 +13,4 @@ $message = $_POST["message"];
 
 
 
-require_once "Mail.php";
-
-$host = "ssl://smtp.gmail.com";
-$username = "noreply.teamgao@gmail.com";
-$password = "P@ssw0rdtoor";
-$port = "587";
-$to = "gao_yujing@columbusstate.edu";
-$email_from = $emailFrom;
-$email_subject = "Subject Line Here: " ;
-$email_body = "whatever you like" ;
-$email_address = $emailFrom;
-
-$headers = array ('From' => $email_from, 'To' => $to, 'Subject' => $email_subject, 'Reply-To' => $email_address);
-$smtp = Mail::factory('smtp', array ('host' => $host, 'port' => $port, 'auth' => true, 'username' => $username, 'password' => $password));
-$mail = $smtp->send($to, $headers, $email_body);
-
-
-if (PEAR::isError($mail)) {
-echo("<p>" . $mail->getMessage() . "</p>");
-} else {
-echo("<p>Message successfully sent!</p>");
-}
-?>
+echo ('Thank you ' . $firstName . '. Your message has been sent to our team.');
