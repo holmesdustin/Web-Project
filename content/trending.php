4c90b09ac1dc5b761dfe9f135ce960967b53a19c
YujingGao <gao.yujing.csu@gmail.com>
2019-10-13 23:55:11 -0400

    test for trending page background

--- content/trending.php
+++ content/trending.php
@@ -4,116 +4,118 @@
     </div>
 </section>
 
-<div class="row container-fluid relative justify-content-center">
-    <div class="col-xs-12 col-sm-12 col-lg-12">
-        <br>
-        <hr class="my-4">
-        <br>
-        <h2 class="junbotron text-center" style="background-color: white">Fun Facts About Halloween</h2>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-lg-9">
-        <div class="bd-example">
-            <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="https://wallpaperaccess.com/full/1102367.jpg" class="d-block w-100" style="border-radius: 20px;" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h3 class="junbotron" style="color: white;">Today's Halloween is a cultural mashup.</h3>
-                            <p>The Halloween holiday we've all come to know and love is a combination of several different celebrations from different cultures and religions at different times in history. The ancient Celtic people celebrated Samhain, marking the end of harvest season and a time when the boundary between the worlds of the living and the dead became blurred and ghosts visited the earth. After the Roman Empire conquered the Celtic peoples, their festivals of Feralia, a day in late October when the Romans honored the passing of the dead, and a day to honor Pomona, the Roman goddess of fruit and trees, were combined with Samhain.</p>
+<div id="trendingBG">
+    <div class="row container-fluid relative justify-content-center">
+        <div class="col-xs-12 col-sm-12 col-lg-12">
+            <br>
+            <hr class="my-4">
+            <br>
+            <h2 class="junbotron text-center" style="background-color: white">Fun Facts About Halloween</h2>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-lg-9">
+            <div class="bd-example">
+                <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="https://wallpaperaccess.com/full/1102367.jpg" class="d-block w-100" style="border-radius: 20px;" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h3 class="junbotron" style="color: white;">Today's Halloween is a cultural mashup.</h3>
+                                <p>The Halloween holiday we've all come to know and love is a combination of several different celebrations from different cultures and religions at different times in history. The ancient Celtic people celebrated Samhain, marking the end of harvest season and a time when the boundary between the worlds of the living and the dead became blurred and ghosts visited the earth. After the Roman Empire conquered the Celtic peoples, their festivals of Feralia, a day in late October when the Romans honored the passing of the dead, and a day to honor Pomona, the Roman goddess of fruit and trees, were combined with Samhain.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://img2.akspic.com/image/63432-farmhouse-full_moon-nature-moonlight-halloween-1920x1080.jpg" class="d-block w-100" style="border-radius: 20px;" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h3 class="junbotron" style="color: white;">Dressing up in costumes was once a way to hide from ghosts.</h3>
+                                <p>The tradition originated as a way for the Celtic and other European people to hide from the spirits who returned at this time of year. People wore masks when they left their homes after dark so the ghosts would think they were fellow spirits. To keep the ghosts out of their houses, people would place bowls of food outside to make them happy.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="http://getwallpapers.com/wallpaper/full/6/2/9/83885.jpg" class="d-block w-100" style="border-radius: 20px;" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h3 class="junbotron" style="color: white;">Jack-o-lanterns were originally carved into turnips.</h3>
+                                <p>In a traditional Celtic story, a man named Jack tricked the Devil, so after Jack died the Devil made him roam the night with only a burning coal to light his way. Jack put the piece of coal in a carved-out turnip, a common vegetable there, and became known as Jack of the Lantern. Irish and Scottish people would carve their own versions of Jack's lantern with scary faces and place them near windows or doors to frighten away Jack or other evil spirits. When immigrants brought the tradition to America, the native pumpkin was more available than turnips, and today's jack-o-lanterns were born.</p>
+                            </div>
                         </div>
                     </div>
-                    <div class="carousel-item">
-                        <img src="https://img2.akspic.com/image/63432-farmhouse-full_moon-nature-moonlight-halloween-1920x1080.jpg" class="d-block w-100" style="border-radius: 20px;" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h3 class="junbotron" style="color: white;">Dressing up in costumes was once a way to hide from ghosts.</h3>
-                            <p>The tradition originated as a way for the Celtic and other European people to hide from the spirits who returned at this time of year. People wore masks when they left their homes after dark so the ghosts would think they were fellow spirits. To keep the ghosts out of their houses, people would place bowls of food outside to make them happy.</p>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="http://getwallpapers.com/wallpaper/full/6/2/9/83885.jpg" class="d-block w-100" style="border-radius: 20px;" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h3 class="junbotron" style="color: white;">Jack-o-lanterns were originally carved into turnips.</h3>
-                            <p>In a traditional Celtic story, a man named Jack tricked the Devil, so after Jack died the Devil made him roam the night with only a burning coal to light his way. Jack put the piece of coal in a carved-out turnip, a common vegetable there, and became known as Jack of the Lantern. Irish and Scottish people would carve their own versions of Jack's lantern with scary faces and place them near windows or doors to frighten away Jack or other evil spirits. When immigrants brought the tradition to America, the native pumpkin was more available than turnips, and today's jack-o-lanterns were born.</p>
-                        </div>
-                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
                 </div>
-                <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
             </div>
         </div>
     </div>
-</div>
 
 
 
-<div class="row container-fluid relative">
-    <div class="col-xs-12 col-sm-12 col-lg-12">
-        <br>
-        <hr class="my-4">
-        <br>
-        <h2 class="junbotron text-center" style="background-color: white">Trending Halloween Movies</h2>
-    </div>
-    <div class="col-xs-12 col-sm-6 col-lg-3">
-        <div class="card shadow text-center" style="border-radius: 20px; height: 90%; width: 100%;">
-            <img class="card-img-top img-fluid" style="border-radius: 20px 20px 0px 0px;" src="https://m.media-amazon.com/images/M/MV5BNzk1OGU2NmMtNTdhZC00NjdlLWE5YTMtZTQ0MGExZTQzOGQyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg" alt="BG">
-            <div class="card-body">
-                <h4 class="card-title">Halloween - 1978</h5>
-                    <p class="card-text">Fifteen years after murdering his sister on Halloween night 1963, Michael Myers escapes from a mental hospital and returns to the small town of Haddonfield, Illinois to kill again.</p>
-            </div>
+    <div class="row container-fluid relative">
+        <div class="col-xs-12 col-sm-12 col-lg-12">
+            <br>
+            <hr class="my-4">
+            <br>
+            <h2 class="junbotron text-center" style="background-color: white">Trending Halloween Movies</h2>
         </div>
-        <br>
-        <hr class="my-4">
-        <br>
-    </div>
-
-    <div class="col-xs-12 col-sm-6 col-lg-3">
-        <div class="card shadow text-center" style="border-radius: 20px; height: 90%; width: 100%;">
-            <img class="card-img-top img-fluid" style="border-radius: 20px 20px 0px 0px;" src="https://m.media-amazon.com/images/M/MV5BMmQyYmY5ZTMtM2JkNi00NmM2LWE3ZmEtYWYzZmRkZDM0ZTdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg" alt="BG">
-            <div class="card-body">
-                <h4 class="card-title">Hocus Pocus - 1993</h5>
-                    <p class="card-text">A curious youngster moves to Salem, where he struggles to fit in before awakening a trio of diabolical witches that were executed in the 17th century.</p>
+        <div class="col-xs-12 col-sm-6 col-lg-3">
+            <div class="card shadow text-center" style="border-radius: 20px; height: 90%; width: 100%;">
+                <img class="card-img-top img-fluid" style="border-radius: 20px 20px 0px 0px;" src="https://m.media-amazon.com/images/M/MV5BNzk1OGU2NmMtNTdhZC00NjdlLWE5YTMtZTQ0MGExZTQzOGQyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg" alt="BG">
+                <div class="card-body">
+                    <h4 class="card-title">Halloween - 1978</h5>
+                        <p class="card-text">Fifteen years after murdering his sister on Halloween night 1963, Michael Myers escapes from a mental hospital and returns to the small town of Haddonfield, Illinois to kill again.</p>
+                </div>
             </div>
+            <br>
+            <hr class="my-4">
+            <br>
         </div>
-        <br>
-        <hr class="my-4">
-        <br>
-    </div>
 
-    <div class="col-xs-12 col-sm-6 col-lg-3">
-        <div class="card shadow text-center" style="border-radius: 20px; height: 90%; width: 100%;">
-            <img class="card-img-top img-fluid" style="border-radius: 20px 20px 0px 0px;" src="https://m.media-amazon.com/images/M/MV5BMTM3NjQyODI3M15BMl5BanBnXkFtZTcwMDM4NjM0OA@@._V1_SX300.jpg" alt="BG">
-            <div class="card-body">
-                <h4 class="card-title">Hotel Transylvania - 2012</h5>
-                    <p class="card-text">Dracula, who operates a high-end resort away from the human world, goes into overprotective mode when a boy discovers the resort and falls for the count's teenaged daughter.</p>
+        <div class="col-xs-12 col-sm-6 col-lg-3">
+            <div class="card shadow text-center" style="border-radius: 20px; height: 90%; width: 100%;">
+                <img class="card-img-top img-fluid" style="border-radius: 20px 20px 0px 0px;" src="https://m.media-amazon.com/images/M/MV5BMmQyYmY5ZTMtM2JkNi00NmM2LWE3ZmEtYWYzZmRkZDM0ZTdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg" alt="BG">
+                <div class="card-body">
+                    <h4 class="card-title">Hocus Pocus - 1993</h5>
+                        <p class="card-text">A curious youngster moves to Salem, where he struggles to fit in before awakening a trio of diabolical witches that were executed in the 17th century.</p>
+                </div>
             </div>
+            <br>
+            <hr class="my-4">
+            <br>
         </div>
-        <br>
-        <hr class="my-4">
-        <br>
-    </div>
 
-    <div class="col-xs-12 col-sm-6 col-lg-3">
-        <div class="card shadow text-center" style="border-radius: 20px; height: 90%; width: 100%;">
-            <img class="card-img-top img-fluid" style="border-radius: 20px 20px 0px 0px;" src="https://m.media-amazon.com/images/M/MV5BOTAyYjQ3NjctNGMzNi00YThkLThmYzUtZDViYWZkMDA2YTMyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjAwMjc0NjQ@._V1_SX300.jpg" alt="BG">
-            <div class="card-body">
-                <h4 class="card-title">Halloweentown - 1998</h5>
-                    <p class="card-text">After learning she is a witch, a girl helps save a town full of other supernatural creatures.</p>
+        <div class="col-xs-12 col-sm-6 col-lg-3">
+            <div class="card shadow text-center" style="border-radius: 20px; height: 90%; width: 100%;">
+                <img class="card-img-top img-fluid" style="border-radius: 20px 20px 0px 0px;" src="https://m.media-amazon.com/images/M/MV5BMTM3NjQyODI3M15BMl5BanBnXkFtZTcwMDM4NjM0OA@@._V1_SX300.jpg" alt="BG">
+                <div class="card-body">
+                    <h4 class="card-title">Hotel Transylvania - 2012</h5>
+                        <p class="card-text">Dracula, who operates a high-end resort away from the human world, goes into overprotective mode when a boy discovers the resort and falls for the count's teenaged daughter.</p>
+                </div>
             </div>
+            <br>
+            <hr class="my-4">
+            <br>
         </div>
-        <br>
-        <hr class="my-4">
-        <br>
-    </div>
 
+        <div class="col-xs-12 col-sm-6 col-lg-3">
+            <div class="card shadow text-center" style="border-radius: 20px; height: 90%; width: 100%;">
+                <img class="card-img-top img-fluid" style="border-radius: 20px 20px 0px 0px;" src="https://m.media-amazon.com/images/M/MV5BOTAyYjQ3NjctNGMzNi00YThkLThmYzUtZDViYWZkMDA2YTMyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjAwMjc0NjQ@._V1_SX300.jpg" alt="BG">
+                <div class="card-body">
+                    <h4 class="card-title">Halloweentown - 1998</h5>
+                        <p class="card-text">After learning she is a witch, a girl helps save a town full of other supernatural creatures.</p>
+                </div>
+            </div>
+            <br>
+            <hr class="my-4">
+            <br>
+        </div>
+
+    </div>
 </div>
