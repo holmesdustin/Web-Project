1e3bd5b67b42032398726f43dc7be05500ffae5d
YujingGao <gao.yujing.csu@gmail.com>
2019-10-19 22:05:10 -0400

    add  frame for testing

--- content/trending.php
+++ content/trending.php
@@ -30,7 +30,7 @@
                             </div>
                         </div>
                         <div class="carousel-item">
-                            <img src="https://img2.akspic.com/image/63432-farmhouse-full_moon-nature-moonlight-halloween-1920x1080.jpg" class="d-block w-100" style="border-radius: 20px;" alt="...">
+                            <img src="https://secure.cdn1.wdpromedia.com/resize/mwImage/1/1920/1080/75/dam/wdpro-assets/dlr/parks-and-tickets/special-events/halloween-time-at-the-disneyland-resort/v2/oogie-boogie-halloween-time-16x9.jpg?1546977301877" class="d-block w-100" style="border-radius: 20px;" alt="...">
                             <div class="carousel-caption d-none d-md-block">
                                 <h3 class="junbotron" style="color: white;">Dressing up in costumes was once a way to hide from ghosts.</h3>
                                 <p>The tradition originated as a way for the Celtic and other European people to hide from the spirits who returned at this time of year. People wore masks when they left their homes after dark so the ghosts would think they were fellow spirits. To keep the ghosts out of their houses, people would place bowls of food outside to make them happy.</p>
@@ -119,4 +119,8 @@
         </div>
 
     </div>
+</div>
+
+<div id="pumpkinFrame">
+
 </div>
