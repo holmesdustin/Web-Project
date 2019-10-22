c77e148f4b0705ae26c688cde9795f89089adb21
YujingGao <gao.yujing.csu@gmail.com>
2019-10-21 23:01:33 -0400

    resize recaptcha

--- template/template.php
+++ template/template.php
@@ -156,13 +156,36 @@
                             alert("Failed to reach server. Please try again.");
                         }
                     });
-                    
+
                 }
             }
             return false;
         });
     </script>
+    <script>
+        $(function() {
+            function rescaleCaptcha() {
+                var width = $('.g-recaptcha').parent().width();
+                var scale;
+                if (width < 302) {
+                    scale = width / 302;
+                } else {
+                    scale = 1.0;
+                }
 
+                $('.g-recaptcha').css('transform', 'scale(' + scale + ')');
+                $('.g-recaptcha').css('-webkit-transform', 'scale(' + scale + ')');
+                $('.g-recaptcha').css('transform-origin', '0 0');
+                $('.g-recaptcha').css('-webkit-transform-origin', '0 0');
+            }
+
+            rescaleCaptcha();
+            $(window).resize(function() {
+                rescaleCaptcha();
+            });
+
+        });
+    </script>
 </body>
 
 </html>
