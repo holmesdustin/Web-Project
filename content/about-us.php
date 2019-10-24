efe3f0c29ff9d029d3cc67232ed8454dc5ce7c7d
YujingGao <gao.yujing.csu@gmail.com>
2019-10-24 17:18:26 -0400

    update social media link for Yujing

--- content/about-us.php
+++ content/about-us.php
@@ -69,10 +69,10 @@
                     <div class="card-body">
                         <h4 class="card-title" style="color:white">Yujing Gao</h4>
                             <p class="card-text">PHP Programmer and Code Technician<br />Team Gao 2019</p>
-                            <a href="#" class="icon fa-twitter fa-2x"><span class="label">Twitter</span></a>&nbsp; &nbsp;
-                            <a href="#" class="icon fa-facebook fa-2x"><span class="label">Facebook</span></a>&nbsp; &nbsp;
-                            <a href="#" class="icon fa-instagram fa-2x"><span class="label">Instagram</span></a>&nbsp; &nbsp;
-                            <a href="#" class="icon fa-github fa-2x"><span class="label">Github</span></a>&nbsp; &nbsp;
+                            <a href="https://twitter.com/gao_yujing" class="icon fa-twitter fa-2x"><span class="label">Twitter</span></a>&nbsp; &nbsp;
+                            <a href="https://www.facebook.com/yujing.gao.16" class="icon fa-facebook fa-2x"><span class="label">Facebook</span></a>&nbsp; &nbsp;
+                            <a href="https://www.instagram.com/jcl_is_the_best_language/" class="icon fa-instagram fa-2x"><span class="label">Instagram</span></a>&nbsp; &nbsp;
+                            <a href="https://github.com/YujingGao96" class="icon fa-github fa-2x"><span class="label">Github</span></a>&nbsp; &nbsp;
                     </div>
                 </div>
                 <br />
