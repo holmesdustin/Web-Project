23073ef6bda0ebb2a072d51833392946a934b0db
YujingGao <gao.yujing.csu@gmail.com>
2019-10-24 17:25:26 -0400

    update some close tags

--- content/about-us.php
+++ content/about-us.php
@@ -18,99 +18,100 @@
 
         <h2 id="about-us-heading-1" class="text-center">Who is Team Gao?</h2>
         <p class="about-us-description">Team Gao was started for our Server Side Web Development group project. We were tasked with creating a movie seraching website powered by the OMDb API.
-           Team Gao consists of four members of the Columbus State University Computer Science department: Caleb Corbin, Yujing Gao, Dustin Holmes, and Johnathan Stadtmueller.
-           We have been friends and classmates for most of the time that we have been at Columbus State University.
-           We believe that we work well together and hope that this website is a good representation of such.
-           Read below for a little bit more information about each individual team member.</p>
+            Team Gao consists of four members of the Columbus State University Computer Science department: Caleb Corbin, Yujing Gao, Dustin Holmes, and Johnathan Stadtmueller.
+            We have been friends and classmates for most of the time that we have been at Columbus State University.
+            We believe that we work well together and hope that this website is a good representation of such.
+            Read below for a little bit more information about each individual team member.</p>
 
 
-        <h3 class="about-us-member">😀  Caleb Corbin</h3>
+        <h3 class="about-us-member">😀 Caleb Corbin</h3>
         <p class="about-us-description">What's up guys? My name is Caleb, and I am a senior in the Games Programming track here at Columbus State University.
-         I am also working on the Web Development certificate, hence why I am in this class.
-         I should be graduating in May 2020, and I am hoping to begin my career after college doing Web Development in Colorado.
-         My biggest interests aside from technology are playing guitar, singing, writing songs, hiking, camping, landscape photography, and travelling.</p>
+            I am also working on the Web Development certificate, hence why I am in this class.
+            I should be graduating in May 2020, and I am hoping to begin my career after college doing Web Development in Colorado.
+            My biggest interests aside from technology are playing guitar, singing, writing songs, hiking, camping, landscape photography, and travelling.</p>
 
-        <h3 class="about-us-member">😁  Yujing Gao</h3>
+        <h3 class="about-us-member">😁 Yujing Gao</h3>
         <p class="about-us-description"> My name is Yujing. I am currently a senior studying Computer Science - Applied Computing at Columbus State University.
             I am planning to graduate in May 2020 as well. After that, I will be looking for a job at either Aflac or TSYS.
             In my spare time, I love to go to the gym with some of my friends. And I also love watching YouTube videos and listening to music in my free time.
         </p>
 
-        <h3 class="about-us-member">😆  Dustin Holmes</h3>
+        <h3 class="about-us-member">😆 Dustin Holmes</h3>
         <p class="about-us-description"> I am Dustin. I love hunting geese 🦆. I play drums. Im a vape God! and I tought Gao all he knows.
-        I will graduate in 2020 and plan to work anywhere with good pay!</p>
+            I will graduate in 2020 and plan to work anywhere with good pay!</p>
 
-        <h3 class="about-us-member">😍  Johnathan Stadtmueller</h3>
+        <h3 class="about-us-member">😍 Johnathan Stadtmueller</h3>
         <p class="about-us-description"> My name is Johnathan, and I like watching "videos". Wants to tech English in CHina after college😍</p>
 
-        <br /><hr class="my-4"><br />
+        <br />
+        <hr class="my-4" /><br />
         <h2 id="about-us-heading-2" class="text-center">Team members</h2>
         <div class="row container-fluid relative">
             <div class="col-xs-12 col-sm-6 col-lg-3">
                 <div class="card shadow-lg text-center" style="border-radius: 20px; background-color: #3b3b3b; height: 80%; width: auto;">
-                    <img class="card-img-top" style="border-radius: 20px 20px 0px 0px;" src="https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Caleb Corbin BG">
+                    <img class="card-img-top" style="border-radius: 20px 20px 0px 0px;" src="https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Caleb Corbin BG" />
                     <div class="card-body">
                         <h4 class="card-title" style="color:white">Caleb Corbin</h4>
-                            <p class="card-text">Content Creation and Website Design<br />Team Gao 2019</p>
-                            <a href="https://twitter.com/dawgsfan97" class="icon fa-twitter fa-2x"><span class="label">Twitter</span></a>&nbsp; &nbsp;
-                            <a href="https://www.facebook.com/caleb.corbin.754" class="icon fa-facebook fa-2x"><span class="label">Facebook</span></a>&nbsp; &nbsp;
-                            <a href="https://www.instagram.com/corbin_caleb/" class="icon fa-instagram fa-2x"><span class="label">Instagram</span></a>&nbsp; &nbsp;
-                            <a href="https://github.com/dawgsfan97" class="icon fa-github fa-2x"><span class="label">Github</span></a>&nbsp; &nbsp;
+                        <p class="card-text">Content Creation and Website Design<br />Team Gao 2019</p>
+                        <a href="https://twitter.com/dawgsfan97" class="icon fa-twitter fa-2x"><span class="label">Twitter</span></a>&nbsp; &nbsp;
+                        <a href="https://www.facebook.com/caleb.corbin.754" class="icon fa-facebook fa-2x"><span class="label">Facebook</span></a>&nbsp; &nbsp;
+                        <a href="https://www.instagram.com/corbin_caleb/" class="icon fa-instagram fa-2x"><span class="label">Instagram</span></a>&nbsp; &nbsp;
+                        <a href="https://github.com/dawgsfan97" class="icon fa-github fa-2x"><span class="label">Github</span></a>&nbsp; &nbsp;
                     </div>
                 </div>
                 <br />
-                <hr class="my-4">
+                <hr class="my-4" />
                 <br />
             </div>
 
             <div class="col-xs-12 col-sm-6 col-lg-3">
                 <div class="card shadow-lg text-center" style="border-radius: 20px; background-color: #3b3b3b; height: 80%; width: auto;">
-                    <img class="card-img-top" style="border-radius: 20px 20px 0px 0px;" src="https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616__340.jpg" alt="Yujing Gao BG">
+                    <img class="card-img-top" style="border-radius: 20px 20px 0px 0px;" src="https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616__340.jpg" alt="Yujing Gao BG" />
                     <div class="card-body">
                         <h4 class="card-title" style="color:white">Yujing Gao</h4>
-                            <p class="card-text">PHP Programmer and Code Technician<br />Team Gao 2019</p>
-                            <a href="https://twitter.com/gao_yujing" class="icon fa-twitter fa-2x"><span class="label">Twitter</span></a>&nbsp; &nbsp;
-                            <a href="https://www.facebook.com/yujing.gao.16" class="icon fa-facebook fa-2x"><span class="label">Facebook</span></a>&nbsp; &nbsp;
-                            <a href="https://www.instagram.com/jcl_is_the_best_language/" class="icon fa-instagram fa-2x"><span class="label">Instagram</span></a>&nbsp; &nbsp;
-                            <a href="https://github.com/YujingGao96" class="icon fa-github fa-2x"><span class="label">Github</span></a>&nbsp; &nbsp;
+                        <p class="card-text">PHP Programmer and Code Technician<br />Team Gao 2019</p>
+                        <a href="https://twitter.com/gao_yujing" class="icon fa-twitter fa-2x"><span class="label">Twitter</span></a>&nbsp; &nbsp;
+                        <a href="https://www.facebook.com/yujing.gao.16" class="icon fa-facebook fa-2x"><span class="label">Facebook</span></a>&nbsp; &nbsp;
+                        <a href="https://www.instagram.com/jcl_is_the_best_language/" class="icon fa-instagram fa-2x"><span class="label">Instagram</span></a>&nbsp; &nbsp;
+                        <a href="https://github.com/YujingGao96" class="icon fa-github fa-2x"><span class="label">Github</span></a>&nbsp; &nbsp;
                     </div>
                 </div>
                 <br />
-                <hr class="my-4">
+                <hr class="my-4" />
                 <br />
             </div>
 
             <div class="col-xs-12 col-sm-6 col-lg-3">
                 <div class="card shadow-lg text-center" style="border-radius: 20px; background-color: #3b3b3b; height: 80%; width: auto;">
-                    <img class="card-img-top" style="border-radius: 20px 20px 0px 0px;" src="https://images.pexels.com/photos/949587/pexels-photo-949587.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Dustin Holmes BG">
+                    <img class="card-img-top" style="border-radius: 20px 20px 0px 0px;" src="https://images.pexels.com/photos/949587/pexels-photo-949587.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Dustin Holmes BG" />
                     <div class="card-body">
                         <h4 class="card-title" style="color:white">Dustin Holmes</h4>
-                            <p class="card-text">Team Leader and Project Organizer<br />Team Gao 2019</p>
-                            <a href="#" class="icon fa-twitter fa-2x"><span class="label">Twitter</span></a> &nbsp; &nbsp;
-                            <a href="#" class="icon fa-facebook fa-2x"><span class="label">Facebook</span></a> &nbsp; &nbsp;
-                            <a href="#" class="icon fa-instagram fa-2x"><span class="label">Instagram</span></a> &nbsp; &nbsp;
-                            <a href="#" class="icon fa-github fa-2x"><span class="label">Github</span></a> &nbsp; &nbsp;
+                        <p class="card-text">Team Leader and Project Organizer<br />Team Gao 2019</p>
+                        <a href="#" class="icon fa-twitter fa-2x"><span class="label">Twitter</span></a> &nbsp; &nbsp;
+                        <a href="#" class="icon fa-facebook fa-2x"><span class="label">Facebook</span></a> &nbsp; &nbsp;
+                        <a href="#" class="icon fa-instagram fa-2x"><span class="label">Instagram</span></a> &nbsp; &nbsp;
+                        <a href="#" class="icon fa-github fa-2x"><span class="label">Github</span></a> &nbsp; &nbsp;
                     </div>
                 </div>
                 <br />
-                <hr class="my-4">
+                <hr class="my-4" />
                 <br />
             </div>
 
             <div class="col-xs-12 col-sm-6 col-lg-3">
                 <div class="card shadow-lg text-center" style="border-radius: 20px; background-color: #3b3b3b; height: 80%; width: auto;">
-                    <img class="card-img-top" style="border-radius: 20px 20px 0px 0px;" src="https://media.istockphoto.com/photos/christmas-lights-defocused-background-bokeh-gold-blue-picture-id877684562?k=6&m=877684562&s=612x612&w=0&h=-WOtRrbuLnM_GlnDT7sw22w-8vZz8aPFasiVgxSG_eY=" alt="Johnathan Stadtmueller BG">
+                    <img class="card-img-top" style="border-radius: 20px 20px 0px 0px;" src="https://media.istockphoto.com/photos/christmas-lights-defocused-background-bokeh-gold-blue-picture-id877684562?k=6&m=877684562&s=612x612&w=0&h=-WOtRrbuLnM_GlnDT7sw22w-8vZz8aPFasiVgxSG_eY=" alt="Johnathan Stadtmueller BG" />
                     <div class="card-body">
                         <h4 class="card-title" style="color:white">J. Stadtmueller</h4>
-                            <p class="card-text">Visual Designer and Layout Creation<br />Team Gao 2019</p>
-                            <a href="#" class="icon fa-twitter fa-2x"><span class="label">Twitter</span></a>&nbsp; &nbsp;
-                            <a href="#" class="icon fa-facebook fa-2x"><span class="label">Facebook</span></a>&nbsp; &nbsp;
-                            <a href="#" class="icon fa-instagram fa-2x"><span class="label">Instagram</span></a>&nbsp; &nbsp;
-                            <a href="#" class="icon fa-github fa-2x"><span class="label">Github</span></a>&nbsp; &nbsp;
+                        <p class="card-text">Visual Designer and Layout Creation<br />Team Gao 2019</p>
+                        <a href="#" class="icon fa-twitter fa-2x"><span class="label">Twitter</span></a>&nbsp; &nbsp;
+                        <a href="#" class="icon fa-facebook fa-2x"><span class="label">Facebook</span></a>&nbsp; &nbsp;
+                        <a href="#" class="icon fa-instagram fa-2x"><span class="label">Instagram</span></a>&nbsp; &nbsp;
+                        <a href="#" class="icon fa-github fa-2x"><span class="label">Github</span></a>&nbsp; &nbsp;
                     </div>
                 </div>
                 <br />
-                <hr class="my-4">
+                <hr class="my-4" />
                 <br />
             </div>
         </div>
