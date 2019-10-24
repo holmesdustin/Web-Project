be6f6fd5151feb94872357d78287be937dd7dc24
YujingGao <gao.yujing.csu@gmail.com>
2019-10-24 01:08:26 -0400

    js

--- template/assets/js/particles.js
+++ template/assets/js/particles.js
@@ -66,7 +66,7 @@ var pJS = function(tag_id, params){
       },
       line_linked: {
         enable: true,
-        distance: 100,
+        distance: 50,
         color: '#fff',
         opacity: 1,
         width: 1
