afaad488dcfaffce0686d1f84abe2957b290c923
holmesdustin <holmes_dustin@columbusstate.edu>
2019-10-24 20:59:37 +0000

    about me

--- content/about-us.php
+++ content/about-us.php
@@ -12,18 +12,18 @@
             <p class="info">Updated on Oct 24, 2019 by <strong>Team Gao</strong></p>
         </header>
         <h2 id="about-us-heading-1" class="text-center">Our Goal</h2>
-        <p class="about-us-description"> Team Gao has one simple goal: to create the best movie searching experience. 
+        <p class="about-us-description"> Team Gao has one simple goal: to create the best movie searching experience.
             We have been working dilligently to deliver this to our users, and
             we highly encourage you to leave any feedback you may have so that we can continue to better our service.</p>
-        
+
         <h2 id="about-us-heading-1" class="text-center">Who is Team Gao?</h2>
         <p class="about-us-description">Team Gao was started for our Server Side Web Development group project. We were tasked with creating a movie seraching website powered by the OMDb API.
            Team Gao consists of four members of the Columbus State University Computer Science department: Caleb Corbin, Yujing Gao, Dustin Holmes, and Johnathan Stadtmueller.
            We have been friends and classmates for most of the time that we have been at Columbus State University.
            We believe that we work well together and hope that this website is a good representation of such.
            Read below for a little bit more information about each individual team member.</p>
- 
-        
+
+
         <h3 class="about-us-member">😀  Caleb Corbin</h3>
         <p class="about-us-description">What's up guys? My name is Caleb, and I am a senior in the Games Programming track here at Columbus State University.
          I am also working on the Web Development certificate, hence why I am in this class.
@@ -35,10 +35,11 @@
             I am planning to graduate in May 2020 as well. After that, I will be looking for a job at either Aflac or TSYS.
             In my spare time, I love to go to the gym with some of my friends. And I also love watching YouTube videos and listening to music in my free time.
         </p>
-        
+
         <h3 class="about-us-member">😆  Dustin Holmes</h3>
-        <p class="about-us-description"> I am Dustin. I love hunting geese. Plays drums🦆</p>
-        
+        <p class="about-us-description"> I am Dustin. I love hunting geese 🦆. I play drums. Im a vape God! and I tought Gao all he knows.
+        I will graduate in 2020 and plan to work anywhere with good pay!</p>
+
         <h3 class="about-us-member">😍  Johnathan Stadtmueller</h3>
         <p class="about-us-description"> My name is Johnathan, and I like watching "videos". Wants to tech English in CHina after college😍</p>
 
@@ -49,7 +50,7 @@
                 <div class="card shadow-lg text-center" style="border-radius: 20px; background-color: #3b3b3b; height: 80%; width: auto;">
                     <img class="card-img-top" style="border-radius: 20px 20px 0px 0px;" src="https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Caleb Corbin BG">
                     <div class="card-body">
-                        <h4 class="card-title" style="color:white">Caleb Corbin</h5>
+                        <h4 class="card-title" style="color:white">Caleb Corbin</h4>
                             <p class="card-text">Content Creation and Website Design<br>Team Gao 2019</p>
                             <a href="https://twitter.com/dawgsfan97" class="icon fa-twitter fa-2x"><span class="label">Twitter</span></a>&nbsp; &nbsp;
                             <a href="https://www.facebook.com/caleb.corbin.754" class="icon fa-facebook fa-2x"><span class="label">Facebook</span></a>&nbsp; &nbsp;
@@ -61,12 +62,12 @@
                 <hr class="my-4">
                 <br>
             </div>
-        
+
             <div class="col-xs-12 col-sm-6 col-lg-3">
                 <div class="card shadow-lg text-center" style="border-radius: 20px; background-color: #3b3b3b; height: 80%; width: auto;">
                     <img class="card-img-top" style="border-radius: 20px 20px 0px 0px;" src="https://cdn.pixabay.com/photo/2016/11/29/05/45/astronomy-1867616__340.jpg" alt="Yujing Gao BG">
                     <div class="card-body">
-                        <h4 class="card-title" style="color:white">Yujing Gao</h5>
+                        <h4 class="card-title" style="color:white">Yujing Gao</h4>
                             <p class="card-text">PHP Programmer and Code Technician<br>Team Gao 2019</p>
                             <a href="#" class="icon fa-twitter fa-2x"><span class="label">Twitter</span></a>&nbsp; &nbsp;
                             <a href="#" class="icon fa-facebook fa-2x"><span class="label">Facebook</span></a>&nbsp; &nbsp;
@@ -83,7 +84,7 @@
                 <div class="card shadow-lg text-center" style="border-radius: 20px; background-color: #3b3b3b; height: 80%; width: auto;">
                     <img class="card-img-top" style="border-radius: 20px 20px 0px 0px;" src="https://images.pexels.com/photos/949587/pexels-photo-949587.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="Dustin Holmes BG">
                     <div class="card-body">
-                        <h4 class="card-title" style="color:white">Dustin Holmes</h5>
+                        <h4 class="card-title" style="color:white">Dustin Holmes</h4>
                             <p class="card-text">Team Leader and Project Organizer<br>Team Gao 2019</p>
                             <a href="#" class="icon fa-twitter fa-2x"><span class="label">Twitter</span></a> &nbsp; &nbsp;
                             <a href="#" class="icon fa-facebook fa-2x"><span class="label">Facebook</span></a> &nbsp; &nbsp;
@@ -100,7 +101,7 @@
                 <div class="card shadow-lg text-center" style="border-radius: 20px; background-color: #3b3b3b; height: 80%; width: auto;">
                     <img class="card-img-top" style="border-radius: 20px 20px 0px 0px;" src="https://media.istockphoto.com/photos/christmas-lights-defocused-background-bokeh-gold-blue-picture-id877684562?k=6&m=877684562&s=612x612&w=0&h=-WOtRrbuLnM_GlnDT7sw22w-8vZz8aPFasiVgxSG_eY=" alt="Johnathan Stadtmueller BG">
                     <div class="card-body">
-                        <h4 class="card-title" style="color:white">J. Stadtmueller</h5>
+                        <h4 class="card-title" style="color:white">J. Stadtmueller</h4>
                             <p class="card-text">Visual Designer and Layout Creation<br>Team Gao 2019</p>
                             <a href="#" class="icon fa-twitter fa-2x"><span class="label">Twitter</span></a>&nbsp; &nbsp;
                             <a href="#" class="icon fa-facebook fa-2x"><span class="label">Facebook</span></a>&nbsp; &nbsp;
