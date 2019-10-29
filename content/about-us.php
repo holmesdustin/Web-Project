14db6bae70247c128c3eae26c5074cbd0f5e7f37
holmesdustin <holmes_dustin@columbusstate.edu>
2019-10-29 18:18:02 +0000

    gram

--- content/about-us.php
+++ content/about-us.php
@@ -92,7 +92,7 @@
                         <p class="card-text">Team Leader and Project Organizer<br /><br />&copy;Team Gao 2019</p>
                         <a href="#" class="icon fa-twitter fa-2x"><span class="label">Twitter</span></a> &nbsp; &nbsp;
                         <a href="#" class="icon fa-facebook fa-2x"><span class="label">Facebook</span></a> &nbsp; &nbsp;
-                        <a href="#" class="icon fa-instagram fa-2x"><span class="label">Instagram</span></a> &nbsp; &nbsp;
+                        <a href="https://www.instagram.com/dustin_holmes97/" class="icon fa-instagram fa-2x"><span class="label">Instagram</span></a> &nbsp; &nbsp;
                         <a href="#" class="icon fa-github fa-2x"><span class="label">Github</span></a> &nbsp; &nbsp;
                     </div>
                 </div>
