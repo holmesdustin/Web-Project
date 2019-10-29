5b92812d2129fadcbdddda2ed7ffbecb04755a19
holmesdustin <holmes_dustin@columbusstate.edu>
2019-10-29 18:25:29 +0000

    gram

--- content/contact.php
+++ content/contact.php
@@ -51,10 +51,10 @@
                     <div class="card-body">
                         <h4 class="card-title">Dustin Holmes</h4>
                         <p class="card-text">Team Leader and Project Organizer<br /><br />&copy;Team Gao 2019</p>
-                        <a href="#" class="icon fa-twitter fa-2x"><span class="label">Twitter</span></a>&nbsp; &nbsp;
-                        <a href="#" class="icon fa-facebook fa-2x"><span class="label">Facebook</span></a>&nbsp; &nbsp;
-                        <a href="#" class="icon fa-instagram fa-2x"><span class="label">Instagram</span></a>&nbsp; &nbsp;
-                        <a href="#" class="icon fa-github fa-2x"><span class="label">Github</span></a>&nbsp; &nbsp;
+                        <a href="https://twitter.com/realDonaldTrump?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" class="icon fa-twitter fa-2x"><span class="label">Twitter</span></a>&nbsp; &nbsp;
+                        <a href="https://www.facebook.com/dustin.holmes.75" class="icon fa-facebook fa-2x"><span class="label">Facebook</span></a>&nbsp; &nbsp;
+                        <a href="https://www.instagram.com/dustin_holmes97/" class="icon fa-instagram fa-2x"><span class="label">Instagram</span></a>&nbsp; &nbsp;
+                        <a href="https://github.com/holmesdustin" class="icon fa-github fa-2x"><span class="label">Github</span></a>&nbsp; &nbsp;
                     </div>
                 </div>
                 <br />
